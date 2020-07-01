d22642b1871cc7c0d1fee1592daabe76c4f02541
CTI22751 <fernando.garcia@claro.com.ar>
2020-07-01 15:12:23 -0300

    docs agregado

---
+++ docs/precache-manifest.f05d5fb7430c43e4863cf0e39f5aeed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28e95ac0e96b543942f16d684e99ec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "229c047786361577128b",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "229c047786361577128b",
+    "url": "/static/js/main.bd0307d6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
